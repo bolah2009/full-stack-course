66c85e53b4936c70f976e2d69e0ae0230ae82748
Bola Ahmed Buari <bola_ahmed2002@yahoo.com>
2020-12-25 23:06:40 +0100

    Add part1 question 1.4 solution
    Refactor App component to use array of objects for variable definition

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -10,42 +10,43 @@ const Part = ({ part: { name, exercises } }) => (
   </p>
 );
 
-const Content = ({ part1, part2, part3 }) => (
+const Content = ({ parts }) => (
   <div>
-    <Part part={part1} />
-    <Part part={part2} />
-    <Part part={part3} />
+    <Part part={parts[0]} />
+    <Part part={parts[1]} />
+    <Part part={parts[2]} />
   </div>
 );
 
-const Total = ({ exercises1, exercises2, exercises3 }) => (
-  <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+const Total = ({ parts }) => (
+  <p>
+    Number of exercises{' '}
+    {parts[0].exercises + parts[1].exercises + parts[2].exercises}
+  </p>
 );
 
 const App = () => {
   const course = 'Half Stack application development';
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10,
-  };
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7,
-  };
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14,
-  };
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10,
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7,
+    },
+    {
+      name: 'State of a component',
+      exercises: 14,
+    },
+  ];
 
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1} part2={part2} part3={part3} />
-      <Total
-        exercises1={part1.exercises}
-        exercises2={part2.exercises}
-        exercises3={part3.exercises}
-      />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   );
 };
