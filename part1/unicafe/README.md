d4734685ce2f01f6de973066163e23d81d514046
Bola Ahmed Buari <bola_ahmed2002@yahoo.com>
2020-12-26 00:16:58 +0100

    Add part1 question 1.6 solution
    Generates react app with CRA, remove unneccesary files, creates needed component as requested in the exercise for the unicafe app

---
+++ part1/unicafe/README.md
@@ -0,0 +1,1 @@
+# Fullstackopen Assignment - Unicafe (React)
---
+++ part1/unicafe/src/index.js
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+const Header = ({ title }) => <h1>{title}</h1>;
+const Button = ({ name, handleClick, count }) => (
+  <button onClick={() => handleClick(count + 1)} type='button'>
+    {name}
+  </button>
+);
+
+const ButtonPanel = ({ clicks }) => {
+  const renderButtons = clicks.map(({ name, count, handleClick }, index) => (
+    <Button name={name} key={index} count={count} handleClick={handleClick} />
+  ));
+
+  return <div>{renderButtons}</div>;
+};
+
+const Part = ({ name, count }) => (
+  <p>
+    {name}: {count}
+  </p>
+);
+
+const Statistics = ({ data: { good, neutral, bad } }) => (
+  <div>
+    <Part name='good' count={good} />
+    <Part name='neutral' count={neutral} />
+    <Part name='bad' count={bad} />
+  </div>
+);
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const clicks = [
+    {
+      name: 'good',
+      count: good,
+      handleClick: setGood,
+    },
+    {
+      name: 'neutral',
+      count: neutral,
+      handleClick: setNeutral,
+    },
+    {
+      name: 'bad',
+      count: bad,
+      handleClick: setBad,
+    },
+  ];
+
+  const data = { good, bad, neutral };
+
+  return (
+    <div>
+      <Header title='Give Feedback' />
+      <ButtonPanel clicks={clicks} />
+      <Statistics data={data} />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
