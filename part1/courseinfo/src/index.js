ae27430e8f71f9f541f023827fa5a56594caf03e
Bola Ahmed Buari <bola_ahmed2002@yahoo.com>
2020-12-25 22:50:35 +0100

    Add part1 question 1.2 solution
    Refactor Content component to use Part component as its children component

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -4,10 +4,25 @@ import './index.css';
 
 const Header = ({ course }) => <h1>{course}</h1>;
 
-const Content = ({ part, exercises }) => (
+const Part = ({ part, exercises }) => (
   <p>
     {part} {exercises}
   </p>
+);
+
+const Content = ({
+  part1,
+  exercises1,
+  part2,
+  exercises2,
+  part3,
+  exercises3,
+}) => (
+  <div>
+    <Part part={part1} exercises={exercises1} />
+    <Part part={part2} exercises={exercises2} />
+    <Part part={part3} exercises={exercises3} />
+  </div>
 );
 
 const Total = ({ exercises1, exercises2, exercises3 }) => (
@@ -26,9 +41,14 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} exercises={exercises1} />
-      <Content part={part2} exercises={exercises2} />
-      <Content part={part3} exercises={exercises3} />
+      <Content
+        part1={part1}
+        exercises1={exercises1}
+        part2={part2}
+        exercises2={exercises2}
+        part3={part3}
+        exercises3={exercises3}
+      />
       <Total
         exercises1={exercises1}
         exercises2={exercises2}
