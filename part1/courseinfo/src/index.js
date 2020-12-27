1d7f153e1207de49d5cc8313847e5bc7050f4b75
Bola Ahmed Buari <bola_ahmed2002@yahoo.com>
2020-12-27 11:43:59 +0100

    Add part2 question 2.1 solution
    Refactor App component to use a single child component and dynamically render the Part component

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -12,17 +12,17 @@ const Part = ({ part: { name, exercises } }) => (
 
 const Content = ({ parts }) => (
   <div>
-    <Part part={parts[0]} />
-    <Part part={parts[1]} />
-    <Part part={parts[2]} />
+    {parts.map((part) => (
+      <Part key={part.id} part={part} />
+    ))}
   </div>
 );
 
-const Total = ({ parts }) => (
-  <p>
-    Number of exercises{' '}
-    {parts[0].exercises + parts[1].exercises + parts[2].exercises}
-  </p>
+const Course = ({ course }) => (
+  <div>
+    <Header course={course.name} />
+    <Content parts={course.parts} />
+  </div>
 );
 
 const App = () => {
@@ -32,25 +32,22 @@ const App = () => {
       {
         name: 'Fundamentals of React',
         exercises: 10,
+        id: 1,
       },
       {
         name: 'Using props to pass data',
         exercises: 7,
+        id: 2,
       },
       {
         name: 'State of a component',
         exercises: 14,
+        id: 3,
       },
     ],
   };
 
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  );
+  return <Course course={course} />;
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
