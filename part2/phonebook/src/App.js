67a6f5c55fb6a915d80d0652a68c72383bdb97e2
Bola Ahmed Buari <bola_ahmed2002@yahoo.com>
2020-12-28 00:28:07 +0100

    Add part2 question 2.9 solution
    Implement a search field that can be used to filter the list of people by name

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -8,8 +8,18 @@ const Numbers = ({ persons }) => (
   </div>
 );
 
+const initialPersonsState = [
+  { name: 'Arto Hellas', number: '040-123456', id: '0h8ifq6fsa' },
+  { name: 'Ada Lovelace', number: '39-44-5323523', id: '0x1w9cna24' },
+  { name: 'Buari Bola', number: '81-32-94888', id: '00d4mpe5zi' },
+  { name: 'Dan Abramov', number: '12-43-234345', id: '04djf2luk6' },
+  { name: 'Mary Poppendieck', number: '39-23-6423122', id: '0ixn23ehiu' },
+];
+
 const App = () => {
-  const [persons, setPersons] = useState([]);
+  const [persons, setPersons] = useState(initialPersonsState);
+  const [filteredPersons, setfilteredPersons] = useState([...persons]);
+  const [filter, setFilter] = useState('');
   const [newName, setNewName] = useState({
     firstName: '',
     lastName: '',
@@ -20,7 +30,7 @@ const App = () => {
   const generateID = () =>
     Math.random()
       .toString(36)
-      .replace(/[^a-z]+/g, '')
+      .replace(/[^a-z|\d]+/g, '')
       .substr(0, 10);
 
   const nameAlreadyExist = (value) =>
@@ -49,10 +59,33 @@ const App = () => {
     setNewName({ ...newName, ...{ [`${e.target.name}`]: e.target.value } });
   };
 
+  const filterPersons = (value) => {
+    setfilteredPersons(
+      persons.filter(({ name }) => RegExp(value, 'i').test(name))
+    );
+  };
+
+  const handleFilterChange = ({ target: { value } }) => {
+    const filterValue = value.replace(/[^a-z]+/gi, '');
+    setFilter(filterValue);
+    filterPersons(filterValue);
+  };
+
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h1>Phonebook</h1>
+      <div>
+        <label htmlFor='filter'>Filter Phonebook</label>
+        <input
+          type='text'
+          onChange={(e) => handleFilterChange(e)}
+          id='filter'
+          name='filter'
+          value={filter}
+        />
+      </div>
       <form onSubmit={(e) => handleSubmit(e)}>
+        <h2>Add a new record</h2>
         <div>
           <label htmlFor='firstName'>first name:</label>
           <input
@@ -81,7 +114,7 @@ const App = () => {
             value={number}
             name='number'
             id='number'
-            type="tel"
+            type='tel'
           />
         </div>
         <div>
@@ -89,7 +122,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      <Numbers persons={persons} />
+      <Numbers persons={filteredPersons} />
     </div>
   );
 };
