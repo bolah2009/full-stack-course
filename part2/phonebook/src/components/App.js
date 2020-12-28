338a0ef9748bf57b07d8b928eb16283af5be4ba6
Bola Ahmed Buari <bola_ahmed2002@yahoo.com>
2020-12-28 16:07:50 +0100

    Add part2 question 2.11 solution
    Modify the application such that the initial state of the data is fetched from the server using the axios-library

--- part2/phonebook/src/components/App.js
+++ part2/phonebook/src/components/App.js
@@ -1,18 +1,11 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Persons from './Persons';
 import Filter from './Filter';
 import PersonForm from './PersonForm';
 
-const initialPersonsState = [
-  { name: 'Arto Hellas', number: '040-123456', id: '0h8ifq6fsa' },
-  { name: 'Ada Lovelace', number: '39-44-5323523', id: '0x1w9cna24' },
-  { name: 'Buari Bola', number: '81-32-94888', id: '00d4mpe5zi' },
-  { name: 'Dan Abramov', number: '12-43-234345', id: '04djf2luk6' },
-  { name: 'Mary Poppendieck', number: '39-23-6423122', id: '0ixn23ehiu' },
-];
-
 const App = () => {
-  const [persons, setPersons] = useState(initialPersonsState);
+  const [persons, setPersons] = useState([]);
   const [filteredPersons, setfilteredPersons] = useState([...persons]);
   const [filter, setFilter] = useState('');
   const [newRecord, setNewRecord] = useState({
@@ -58,6 +51,12 @@ const App = () => {
     const filterValue = value.replace(/[^a-z| ]+/gi, '');
     setFilter(filterValue);
   };
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/phonebook')
+      .then(({ data }) => setPersons(data));
+  }, []);
 
   useEffect(() => {
     setfilteredPersons(
